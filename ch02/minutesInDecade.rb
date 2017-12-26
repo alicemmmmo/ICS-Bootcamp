c4f9e5f64ac6eae71c9c662f7b88e6e1724b3b87
Alice Mo <alicemo@berkeley.edu>
2017-12-26 15:26:33 -0800

    add files

--- ch02/minutesInDecade.rb
+++ ch02/minutesInDecade.rb
@@ -1,1 +1,5 @@
-puts 60*24*365.25*10
+puts 'Two leap years in a decade'
+puts 60*24*365*10+60*24*2
+puts ' '
+puts 'Three leap years in a decade'
+puts 60*24*365*10+60*24*3
--- ch05/biggerBetterFavoriteNumber.rb
+++ ch05/biggerBetterFavoriteNumber.rb
@@ -1,5 +1,5 @@
 puts 'What\'s your favorite number?'
 number=gets.chomp
-bettter= number.to_i + 1
-better= bettter.to_s
+better_num= number.to_i + 1
+better= better_num.to_s
 puts 'You like number '+ number+ '? I think ' +better+ ' is bigger and better.'
