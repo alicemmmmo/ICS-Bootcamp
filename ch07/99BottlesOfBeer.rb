ddf6a028e8b0d549042222fdb4ed51046747094d
Alice Mo <alicemo@berkeley.edu>
2018-01-05 23:12:56 -0800

    update2

--- ch07/99BottlesOfBeer.rb
+++ ch07/99BottlesOfBeer.rb
@@ -4,12 +4,18 @@ while bottles != 0
 	bottles -= 1
 	puts 'Take one down and pass it around, ' +bottles.to_s+ ' bottles of beer on the wall.'
 	puts ' '
-if bottles == 1
-	puts bottles.to_s + ' bottle of beer on the wall, ' +bottles.to_s + ' bottle of beer.' 
-	puts 'Take one down and pass it around, no more bottles of beer on the wall.'
-	puts ' '
-	bottles -= 1
-	end
+	if bottles == 2
+		puts bottles.to_s + ' bottle of beer on the wall, ' +bottles.to_s + ' bottle of beer.' 
+		puts 'Take one down and pass it around, 1 bottle of beer on the wall.'
+		puts ' '
+		bottles -= 1
+		end
+	if bottles == 1
+		puts bottles.to_s + ' bottle of beer on the wall, ' +bottles.to_s + ' bottle of beer.' 
+		puts 'Take one down and pass it around, no more bottles of beer on the wall.'
+		puts ' '
+		bottles -= 1
+		end
 end
 puts 'No more bottles of beer on the wall, no more bottles of beer. 
 Go to the store and buy some more, 99 bottles of beer on the wall.'
